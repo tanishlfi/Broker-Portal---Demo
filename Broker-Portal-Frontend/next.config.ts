5d2faecbb9546ee559dc3798125e50cec1699ec6
tanishlfi <tdas@linkfields.com>
2026-04-14 18:03:59 +0530

    Update next.config.ts

--- Broker-Portal-Frontend/next.config.ts
+++ Broker-Portal-Frontend/next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  basePath: "/brokerPortal",
-  assetPrefix:"/brokerPortal-static"
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
