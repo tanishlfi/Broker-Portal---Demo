e98b243cf2dacb6bcc5471df03b72d7b4c35f7b7
tanishlfi <tdas@linkfields.com>
2026-04-17 17:04:35 +0530

    Remove login page from Broker Portal Frontend

--- Broker-Portal-Frontend/app/page.tsx
+++ Broker-Portal-Frontend/app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 import { ROUTES } from "@/lib/constants";
 
 export default function Home() {
-  redirect(ROUTES.login);
+  redirect(ROUTES.dashboard);
 }
